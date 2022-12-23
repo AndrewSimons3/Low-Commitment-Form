4420ff5d9bd0c3de3f37c3b9f312411c928956cd
AndrewSimons3 <simdrew07@gmail.com>
2022-12-23 12:23:34 -0700

    done

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-console.log('hello world');
 
-const thankYou = document.querySelector('.thank-you');
-const form = document.querySelector('.lc-form');
-const submitBtn = document.querySelector('#submit');
-
-form.addEventListener('submit', function (e) {
-	e.preventDefault();
-	form.style.display = 'none';
-	thankYou.style.display = 'block';
-});
